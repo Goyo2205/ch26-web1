1a350bce4fbf939dba6d67ee8561cf82773c5472
goyo2205 <jgregorio.ef@gmail.com>
2023-04-10 14:18:51 -0600

    feat: Cambia el saludo en h1

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,15 @@ console.log("mensaje de log");
 
 function changeGretting(){
     const clientName = getNameOfClient();
-    console.log("mmm");
+    //obtener referencia de label h1
+    const grettingReference = getReferenceOfIdGrettings();
+    grettingReference.innerHTML = "Hola " + clientName;
 }
 
 function getNameOfClient(){
     const clientName = prompt("Escribe tu nombre");
     return clientName;
+}
+function getReferenceOfIdGrettings(){
+    return document.getElementById("Gretting");
 }
